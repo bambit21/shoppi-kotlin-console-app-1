0fa5f3ea4bff5abf4b579617cf1f905cbf39dd73
gorani16 <jgee0116@naver.com>
2022-05-16 01:26:44 +0900

    실습 3-2. Home 화면과 Category 화면으로 분리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,12 @@
+import screen.ShoppingHome
+
+/*
+Step 1. main 함수 -> 클래스의 함수
+Step 2. 화면별 클래스 분리
+Step 3. 선택한 카테고리 상품 목록 표시
+*/
+
 fun main(args: Array<String>) {
-    println("안녕하세요, Shoppi 에 오신 것을 환영합니다!")
-    println("쇼핑을 계속 하시려면 이름을 입력해주세요 :)")
-
-    val name = readLine()
-
-    println("""
-        감사합니다. 반갑습니다, $name 님!
-        원하시는 카테고리를 입력해주세요.
-        ***=========================================***
-    """.trimIndent())
-     /*
-     Step 1. 상품 카테고리 표기
-     Step 2. 사용자 입력 받기
-     Step 3. 사용자가 기대하는 값을 입력하지 않는다면?
-     */
-
-    val categories = arrayOf("패션", "전자기기", "반려동물용품")
-    for (category in categories) {
-        println(category)
-    }
-    println("=> 장바구니로 이동하시려면 #을 입력해주세요.")
-
-    var selectedCategory = readLine()
-    // readLine() 은 nullable String 타입이다.
-    // 즉, 입력값을 읽어들이지 못하면 null 이 될 수 있다.
-    // 또한 사용자는 아무것도 입력하지않고 화이트 스페이스만 입력할수도 있다.
-    // 이를 모두 대응하는 방법은 사용자가 어떤 값이든 무언가를 입력할 때까지 재요청하는 것이다.
-    while (selectedCategory.isNullOrBlank()) {
-        println("값을 입력해주세요.")
-        selectedCategory = readLine()
-    }
-
-    if (selectedCategory == "#") {
-        // TODO 1. 장바구니 이동
-    } else {
-        // TODO 2. 카테고리 상품 목록 보여주기
-        // TODO 3. 카테고리 목록에 없는 값을 입력하는 경우
-    }
-
+    val shoppingHome = ShoppingHome()
+    shoppingHome.start()
 }
---
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -0,0 +1,37 @@
+package screen
+
+class ShoppingCategory {
+
+    fun showCategories() {
+        val categories = arrayOf("패션", "전자기기", "반려동물용품")
+        for (category in categories) {
+            println(category)
+        }
+        println("=> 장바구니로 이동하시려면 #을 입력해주세요.")
+
+        var selectedCategory = readLine()
+        // readLine() 은 nullable String 타입이다.
+        // 즉, 입력값을 읽어들이지 못하면 null 이 될 수 있다.
+        // 또한 사용자는 아무것도 입력하지않고 화이트 스페이스만 입력할수도 있다.
+        // 이를 모두 대응하는 방법은 사용자가 어떤 값이든 무언가를 입력할 때까지 재요청하는 것이다.
+        while (selectedCategory.isNullOrBlank()) {
+            println("값을 입력해주세요.")
+            selectedCategory = readLine()
+        }
+
+        if (selectedCategory == "#") {
+            // TODO 1. 장바구니 이동
+        } else {
+            if (categories.contains(selectedCategory)) {
+                // TODO 2. 카테고리 상품 목록 보여주기
+            } else {
+                showErrorMessage(selectedCategory)
+            }
+        }
+    }
+
+    private fun showErrorMessage(selectedCategory: String?) {
+        println("[$selectedCategory] : 존재하지 않는 카테고리입니다. 다시 입력해주세요.")
+        showCategories()
+    }
+}
